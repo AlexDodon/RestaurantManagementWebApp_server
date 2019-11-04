ee37324c00d71b1c0c0623e5dd27bf0e52ba6178
Alex Dodon <dodon.alex@gmail.com>
2019-11-04 14:33:38 +0200

    Initial commit

---
+++ src/main/java/com/Dodon/Gatea/Saas/SaasApplication.java
@@ -0,0 +1,13 @@
+package com.Dodon.Gatea.Saas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaasApplication.class, args);
+	}
+
+}
